28bc9911403f67fab6ed32fc6a68959594b8f1c1
unknown <guilisk8@gmail.com>
2015-03-14 22:35:54 -0300

    NOT WORKING YET - add classes to the slides

--- src/simple-slider.js
+++ src/simple-slider.js
@@ -5,11 +5,22 @@
 // (function(){
 
 	function simpleSlider(config){
+		// Override properties
 		if(config.wrapElement){ this.wrapElement = config.wrapElement; }
 		if(config.slideElement){ this.slideElement = config.slideElement; }
+		this.totalElements = document.getElementsByClassName(this.slideElement).length;
+
+		// Add classes to the slides
+		var slides = document.getElementsByClassName(this.slideElement);
+
+		for (var i = 0; i < slides.length; i++) {
+		    slides[i].className = slides[i].className+' slide-'+(i+1);
+		}
+
 	}
 
-	simpleSlider.prototype.wrapElement = '#slider';
-	simpleSlider.prototype.slideElement = '.slide';
+	simpleSlider.prototype.wrapElement = 'slider';
+	simpleSlider.prototype.slideElement = 'slide';
+	simpleSlider.prototype.totalElements = 0;
 
 // });
