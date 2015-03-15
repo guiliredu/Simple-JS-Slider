e82ef744ecfed39a0603147e6cf549acd8e16664
unknown <guilisk8@gmail.com>
2015-03-14 23:22:04 -0300

    NOT WORKING YET - commit #3

--- src/simple-slider.js
+++ src/simple-slider.js
@@ -22,5 +22,32 @@
 	simpleSlider.prototype.wrapElement = 'slider';
 	simpleSlider.prototype.slideElement = 'slide';
 	simpleSlider.prototype.totalElements = 0;
+	simpleSlider.prototype.slideDelay = 1000;
+	simpleSlider.prototype.slide = 1;
+	simpleSlider.prototype.slideInterval = null;
+
+	simpleSlider.prototype.slideInit = function(){
+		clearInterval(this.slideInterval);
+
+		this.slideInterval = window.setInterval(this.slidePlus(this), this.slideDelay);
+	}
+
+	simpleSlider.prototype.slideChange = function(newSlide){
+		document.getElementsByClassName(this.slideElement)[0].style.display = 'none';
+		document.getElementsByClassName('slide-'+newSlide)[0].style.display = 'block';
+	}
+
+	simpleSlider.prototype.slidePlus = function(slider){
+		slider.slide++;
+		console.log(slider.slide);
+
+		if(slider.slide <= slider.totalElements){
+			slider.slideChange(slider.slide);
+		}else{
+			slider.slide = 1;
+			slider.slideChange(slider.slide);
+		}
+	}
+
 
 // });
