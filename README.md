ac68bb258cfe729224b62403445a7a4f0fb9e118
guiliredu <guilisk8@gmail.com>
2015-08-29 14:09:10 -0300

    Previous and next buttons function added

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Simple-JS-Slider
 Just a simples JS slider
 
-Important: This repo is used to learn javascript, not to use in production. Feel free to uset it whatever you want.
+Important: This repo is used to learn javascript, not to use it in production projects. Feel free to uset it whatever you want.
 
 ###Usage
 
@@ -11,12 +11,18 @@ instance the object and call the `slideInit()` method
 var slider = new simpleSlider();
 slider.slideInit();
 ```
-it will assume that you are using an structure like this:
+it will assume that you are using an html structure like this:
 
 ```html
-<div id="slider">
-	<div class="slide fadeInUp animated" style="background-image:url('assets/img/slide-1.jpg');"></div>
+<div id="js-slider">
+	<div class="js-slide fadeInUp animated" style="background-image:url('assets/img/slide-1.jpg');"></div>
 <div>
+```
+and a CSS stylesheet like this:
+
+```css
+#slider { position:relative; }
+#slider .slide { position:absolute }
 ```
 
 You can add parameters to the constructor:
@@ -29,7 +35,7 @@ var slider = new simpleSlider({
 });
 ```
 
-###Todos
+###To-dos
 
 + prev and next buttons
 + bullets with the number of slides
--- src/simple-slider.js
+++ src/simple-slider.js
@@ -1,6 +1,9 @@
 //
 // Simple JS Slider
 // 
+// Author: Guilherme Redü
+// Github: https://github.com/guiliredu/Simple-JS-Slider
+// 
 
 // (function(){
 
@@ -9,6 +12,8 @@
 		if(config.wrapElement){ this.wrapElement = config.wrapElement; }
 		if(config.slideElement){ this.slideElement = config.slideElement; }
 		if(config.slideDelay){ this.slideDelay = config.slideDelay; }
+		if(config.navPrevElement){ this.navPrevElement = config.navPrevElement; }
+		if(config.navNextElement){ this.navNextElement = config.navNextElement; }
 		this.totalElements = document.getElementsByClassName(this.slideElement).length;
 
 		// Add classes to the slides
@@ -23,30 +28,60 @@
 
 	}
 
-	simpleSlider.prototype.wrapElement = 'slider';
-	simpleSlider.prototype.slideElement = 'slide';
+	simpleSlider.prototype.wrapElement = 'js-slider';
+	simpleSlider.prototype.slideElement = 'js-slide';
+	simpleSlider.prototype.navPrevElement = 'js-slide-nav-prev';
+	simpleSlider.prototype.navNextElement = 'js-slide-nav-next';
 	simpleSlider.prototype.totalElements = 0;
 	simpleSlider.prototype.slideDelay = 1000;
 	simpleSlider.prototype.slide = 1;
 	simpleSlider.prototype.slideInterval = null;
 
 	simpleSlider.prototype.slideInit = function(){
+		// if the interval is set, clear
 		clearInterval(this.slideInterval);
 		var self = this
 
+		// create new interval
 		this.slideInterval = window.setInterval(function(){
-			
-			self.slide++;
-
-			if(self.slide <= self.totalElements){
-				self.slideChange(self.slide);
-			}else{
-				self.slide = 1;
-				self.slideChange(self.slide);
-			}
-			// console.log('SLIDE: '+self.slide+' - TOTAL: '+self.totalElements);
-
+			self.slideNavNext(self);
 		}, this.slideDelay);
+
+		// create click events in the nav buttons
+		if(document.getElementsByClassName(this.navNextElement).length > 0){
+			var navNext = document.getElementsByClassName(this.navNextElement)[0];
+			navNext.addEventListener("click", function(){
+				self.slideNavNext(self)
+			});
+		}
+		if(document.getElementsByClassName(this.navPrevElement).length > 0){
+			var navPrev = document.getElementsByClassName(this.navPrevElement)[0];
+			navPrev.addEventListener("click", function(){
+				self.slideNavPrev(self)
+			});
+		}
+	}
+
+	simpleSlider.prototype.slideNavNext = function(self){
+		self.slide++;
+
+		if(self.slide <= self.totalElements){
+			self.slideChange(self.slide);
+		}else{
+			self.slide = 1;
+			self.slideChange(self.slide);
+		}
+	}
+
+	simpleSlider.prototype.slideNavPrev = function(self){
+		self.slide--;
+
+		if(self.slide > 0){
+			self.slideChange(self.slide);
+		}else{
+			self.slide = self.totalElements;
+			self.slideChange(self.slide);
+		}
 	}
 
 	simpleSlider.prototype.slideChange = function(newSlide){
