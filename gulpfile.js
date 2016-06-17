bdbe603ea319d4ccca31eb6f9ad08b99e25721f7
Guilherme Redü <guilisk8@gmail.com>
2016-06-16 22:59:44 -0300

    Added browserSync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+
+gulp.task('watch', ['browserSync', 'js:uglify'], function(){
+  gulp.watch('src/*.js', ['js:uglify']); 
+});
 
 gulp.task('js:uglify', function(){
   return gulp.src('src/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({stream: true}))
 });
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'demo'
+    },
+  })
+})
